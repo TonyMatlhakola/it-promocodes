684d08d81d8344dfd6af71a9961821ca46a150b4
Kamogelo Matlhakola <kamogelo.matlhakola@iotnxt.com>
2021-04-22 16:45:09 +0200

    data update

--- src/Data/Promodata.js
+++ src/Data/Promodata.js
@@ -38,7 +38,7 @@ export function getPromoData() {
         },
       },
       {
-        ServiceName: "Sitecostructor.io2",
+        ServiceName: "Tomrak",
         Description: "Description",
         ViewData: {
           promocode: "PROMOCODE",
@@ -50,7 +50,7 @@ export function getPromoData() {
         },
       },
       {
-        ServiceName: "Sitecostructor.io3",
+        ServiceName: "Errics",
         Description: "Description",
         ViewData: {
           promocode: "PROMOCODE",
@@ -62,7 +62,284 @@ export function getPromoData() {
         },
       },
       {
-        ServiceName: "Sitecostructor.io4",
+        ServiceName: "Praka.ig",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Wiretel",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Rakas tick",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Uberenke",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Peretel",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "PClick",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Telledou",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Babas inc",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Muraka",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Kopia",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Treeks",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+
+      {
+        ServiceName: "Glodie",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },   {
+        ServiceName: "Toyui",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Locatob",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Frosttrik",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+
+      {
+        ServiceName: "Youbee",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Surf.io",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Mugrugu",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "OPhius",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Inc-Tella",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Pro riks",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Montani",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "Yousiko",
+        Description: "Description",
+        ViewData: {
+          promocode: "PROMOCODE",
+          copy:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/body-2@2x.svg",
+          itpromocodes: "1235Promocode1",
+          icon:
+            "https://anima-uploads.s3.amazonaws.com/projects/607f01fbf77cf4788dd0df96/releases/607f0351ead31aeed437978b/img/icon-8@2x.svg",
+        },
+      },
+      {
+        ServiceName: "willows.ig",
         Description: "Description",
         ViewData: {
           promocode: "PROMOCODE",
